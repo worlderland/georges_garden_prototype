aff1fb4ac681a486fdc8c9da700b40c312036649
worlderland <info.alessandrocoraglia@gmail.com>
2019-03-25 13:47:08 +0800

    wiped everything and tried sticky nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+
+//Navigation menu JS
+$(function() {
+    $(window).scroll(function() {
+      var winTop = $(window).scrollTop();
+      if (winTop >= 30) {
+        $("body").addClass("sticky-shrinknav-wrapper");
+      } else{
+        $("body").removeClass("sticky-shrinknav-wrapper");
+      }
+    });
+  });
